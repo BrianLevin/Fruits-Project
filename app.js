2e155ad49fe6637f4590fd74716fe377a2b71e36
BrianLevin <bml201095@gmail.com>
2021-05-02 12:49:50 -0400

    succesfully connected to fruits db server using older depreciated version of code

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
-const { MongoClient } = require("mongodb");
-// Connection URI
-const uri =
-  "mongodb+srv://sample-hostname:27017/?poolSize=20&writeConcern=majority";
-// Create a new MongoClient
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+const MongoClient = require('mongodb').MongoClient;
+
+const assert= require('assert');
+
+const url= 'mongodb://localhost:27017';
+
+const dbName = 'fruitsDB';
+
+const client = new MongoClient(url);
+
+client.connect(function(err){
+assert.equal(null, err);
+console.log('connected successfully to server');
+
+const db= client.db(dbName);
+
+client.close();
+
+})
+
+
+
+// const { MongoClient } = require("mongodb");
+// // Connection URI
+// const uri =
+//   "mongodb+srv://brianlevin:127.0.0.1:27017/?poolSize=20&writeConcern=majority";
+// // Create a new MongoClient
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server
+//     await client.connect();
+//     // Establish and verify connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Connected successfully to server");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
