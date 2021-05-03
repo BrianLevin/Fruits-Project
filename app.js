2dbafa4e1cfe4cf42ac364fa052addc91fd020bf
BrianLevin <bml201095@gmail.com>
2021-05-03 11:02:52 -0400

    made a peoples collection

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-
+// connect mongoose
 mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
 
-
+// blue print structure of data
 const fruitSchema = new mongoose.Schema ({
     name: String,
     rating: Number,
@@ -11,40 +11,41 @@ const fruitSchema = new mongoose.Schema ({
 
 
 });
-const Fruit =  mongoose.model("Fruit", fruitSchema);
 
+
+
+
+// name fruit will apply to schema
+const Fruit =  mongoose.model("Fruit", fruitSchema);
+//   created new fruit document, from  above model, from  above schema
 const fruit = new Fruit ({
     name: "Apple",
     rating: 7,
     review: "Pretty  solid as a fruit"
 
 });
+// saves fruit document into fruits connection inside fruits db
 
-fruit.save();
+// commented out fruit.save for now because it will keep inserted apples into the data base collection
+//fruit.save();
+
+const personSchema = new mongoose.Schema ({
+    name: String,
+    age: Number
+    
+
+
+});
+
+// name fruit will apply to schema
+const Person =  mongoose.model("Person", personSchema);
+//   created new fruit document, from  above model, from  above schema
+const person = new Person ({
+    name: "John",
+    age: 30 
+});
+
+person.save();
 
 
 
-
-
-// const { MongoClient } = require("mongodb");
-// // Connection URI
-// const uri =
-//   "mongodb+srv://brianlevin:127.0.0.1:27017/?poolSize=20&writeConcern=majority";
-// // Create a new MongoClient
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server
-//     await client.connect();
-//     // Establish and verify connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Connected successfully to server");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
